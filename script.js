55e50f259a37bceac47d6297b045f994fd795ecd
Marcella Mendoza <99848492+MarcellaMendoza@users.noreply.github.com>
2022-05-17 05:26:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+//Practice 1
+// const alienName = "Glorp"
+// const recipient = "Dave"
+
+// let message = `Greetings, ${recipient}! Our rocket just landed on your planet.`
+
+
+// console.log(message)
+
+
+//Practice 2
+// const alienName = "Glorp"
+// const recipient = "Dave"
+// const astronaut = "Marcella"
+
+// let message = `
+//               Greetings, ${recipient}! 
+//               Our rocket just landed on your planet.
+//               Sincerely, Astronaut ${astronaut}`
+              
+
+// console.log(message)
+
+//Part 1
+const alienName = "Glorp"
+const recipient = "Dave"
+astronaut = "Marcella"
+
+let message = `Greetings, ${astronaut}! 
+Our rocket just landed on your planet.
+Sincerely ${alienName}`
+
+console.log(message)
+
+
+
+//Part 2
+
+
